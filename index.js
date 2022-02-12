b9043a27f209aec625d708edf906e9a853ad40a4
Rohan Raja <roraja@microsoft.com>
2022-02-12 17:24:02 +0530

    added servdr

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const { Client } = require("@notionhq/client");
 const http = require("http");
+const url = require('url');
+
 
 const PORT = 5007;
+const databaseId = "633c7560e61b4c7baf846454d9c2391b";
+
+const workoutIdMap = {};
 
 const server = http.createServer(async (req, res) => {
   //set the request route
@@ -21,12 +26,66 @@ const server = http.createServer(async (req, res) => {
     res.end();
   }
 
+  else if (req.url.indexOf("/create") !== -1 && req.method === "GET") {
+    //response headers
+
+    const queryObject = url.parse(req.url, true).query;
+    console.log(queryObject);
+
+    console.log("Create Request recieved!");
+    //set the response
+    const workout = queryObject['w'] ;
+
+    await createPage(workoutIdMap[workout]);
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write("Created!");
+    // res.write('["first", "second", "Barbell squats", "Lying dumbell"]');
+    // res.write('{"list": ["first", "second", "Barbell squats", "Lying dumbell"] }');
+    //end the response
+    res.end();
+  }
   // If no route present
   else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "Route not found" }));
   }
 });
+
+const createPage = async (wid, weight) => {
+  const notion = new Client({ auth: process.env.NOTION_API_KEY });
+  const response = await notion.pages.create({
+    parent: {
+      database_id: "a7530807529441e6bcc7e4649f399236",
+    },
+    icon: {
+      type: "emoji",
+      emoji: "🎉",
+    },
+    properties: {
+      Name: {
+        title: [
+          {
+            text: {
+              content: "test from api",
+            },
+          },
+        ],
+      },
+      Workout: {
+        relation: [
+          {
+            id: wid,
+          },
+        ],
+      },
+      "Weight(kg)": {
+        number: 2.5,
+      },
+    },
+  });
+  console.log(response);
+};
 
 server.listen(PORT, () => {
   console.log(`server started on port: ${PORT}`);
@@ -35,7 +94,6 @@ server.listen(PORT, () => {
 async function getList() {
   const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
-  const databaseId = "633c7560e61b4c7baf846454d9c2391b";
   const response = await notion.databases.query({
     database_id: databaseId,
     filter: {
@@ -57,9 +115,13 @@ async function getList() {
   });
 
   const exercises = response.results.map((res) => {
-    return res.properties["Name"].title[0].plain_text;
+    const id = res.id;
+    const name = res.properties["Name"].title[0].plain_text;
+
+    workoutIdMap[name] = id; 
+
+    return name;
   });
-  console.log(exercises);
 
   return exercises;
 }
