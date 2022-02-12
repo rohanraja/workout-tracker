2e31fdadc117e7754be149bb3dac23923abae2ba
Rohan Raja <roraja@microsoft.com>
2022-02-12 15:44:24 +0530

    added servdr

--- index.js
+++ index.js
@@ -1,10 +1,65 @@
-const { Client } = require('@notionhq/client');
+const { Client } = require("@notionhq/client");
+const http = require("http");
 
-const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const PORT = 5007;
 
+const server = http.createServer(async (req, res) => {
+  //set the request route
+  if (req.url === "/api" && req.method === "GET") {
+    //response headers
 
-(async () => {
-    const databaseId = '633c7560e61b4c7baf846454d9c2391b';
-    const response = await notion.databases.retrieve({ database_id: databaseId });
-    console.log(response);
-  })();
+    console.log("Request recieved!");
+    res.writeHead(200, { "Content-Type": "application/json" });
+    //set the response
+
+    const list = await getList();
+    console.log(list);
+    res.write(JSON.stringify(list));
+    // res.write('["first", "second", "Barbell squats", "Lying dumbell"]');
+    // res.write('{"list": ["first", "second", "Barbell squats", "Lying dumbell"] }');
+    //end the response
+    res.end();
+  }
+
+  // If no route present
+  else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`server started on port: ${PORT}`);
+});
+
+async function getList() {
+  const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+  const databaseId = "633c7560e61b4c7baf846454d9c2391b";
+  const response = await notion.databases.query({
+    database_id: databaseId,
+    filter: {
+      or: [
+        {
+          property: "Pick today",
+          checkbox: {
+            equals: true,
+          },
+        },
+      ],
+    },
+    // sorts: [
+    //   {
+    //     property: 'Last ordered',
+    //     direction: 'ascending',
+    //   },
+    // ],
+  });
+
+  const exercises = response.results.map((res) => {
+    return res.properties["Name"].title[0].plain_text;
+  });
+  console.log(exercises);
+
+  return exercises;
+}
